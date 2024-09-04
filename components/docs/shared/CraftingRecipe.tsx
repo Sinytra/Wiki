212bc40e6fc4263db44ef31b534e0cc9d7c49302
Su5eD <su5ed@outlook.com>
2024-09-04 19:52:25 +0200

    Crafting recipe output count

--- components/docs/shared/CraftingRecipe.tsx
+++ components/docs/shared/CraftingRecipe.tsx
@@ -2,21 +2,22 @@ import assets from "@/lib/docs/assets";
 import ItemDisplay from "@/components/docs/shared/ItemDisplay";
 import Image from "next/image";
 import CraftingBackground from '@/components/assets/crafting_background.png';
+import '@south-paw/typeface-minecraft';
 
 interface Props {
   slots: (string | null)[]
   result: string;
+  count?: number;
 }
 
-// TODO Counts
-export default async function CraftingRecipe({slots, result}: Props) {
+export default async function CraftingRecipe({slots, result, count}: Props) {
   const displaySlots = slots.slice(0, Math.min(slots.length, 9));
   const assetSlots = await Promise.all(displaySlots.map(async slot => slot === null ? null : assets.getAssetResource(slot)));
   const resultAsset = await assets.getAssetResource(result);
 
   return (
     <div className="relative">
-      <Image unoptimized priority src={CraftingBackground} alt="background" width={260} height={136} />
+      <Image unoptimized priority src={CraftingBackground} alt="background" width={260} height={136}/>
 
       <div className="absolute top-4 left-4 grid grid-cols-3 gap-1">
         {assetSlots.map((asset, index) => (
@@ -24,7 +25,19 @@ export default async function CraftingRecipe({slots, result}: Props) {
         ))}
       </div>
 
-      { resultAsset && <div className="absolute top-[52px] left-[204px] w-[32px] h-[32px]"><ItemDisplay asset={resultAsset}/></div> }
+      {resultAsset &&
+          <div className="absolute flex justify-center items-center top-[42px] left-[194px] w-[52px] h-[52px]">
+              <div className="relative flex justify-center items-center -m-0.5 p-0.5 w-[36px] h-[36px]">
+                <div>
+                    <ItemDisplay asset={resultAsset}/>
+                </div>
+                {count &&
+                    <span className="absolute bottom-0 right-0 text-white text-base leading-[1] text-left sharpRendering z-10" style={{fontFamily: 'Minecraft, sans-serif', textShadow: '2px 2px 0 #3F3F3F'}}>
+                      {count}
+                    </span>
+                }
+              </div>
+          </div>}
     </div>
   )
 }
