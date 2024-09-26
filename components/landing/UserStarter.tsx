822bab2bca8cdba7e30cf4917115a10975bab093
Su5eD <su5ed@outlook.com>
2024-09-26 17:37:33 +0200

    Fix missing lang provider

--- components/landing/UserStarter.tsx
+++ components/landing/UserStarter.tsx
@@ -3,18 +3,23 @@ import {BookTextIcon, CompassIcon} from "lucide-react";
 import LandingStarterBase from "@/components/landing/LandingStarterBase";
 import ModSearch from "@/components/landing/ModSearch";
 import {LocaleNavLink} from "@/components/navigation/link/LocaleNavLink";
-import {getLocale, getTranslations} from "next-intl/server";
+import {getLocale, getMessages, getTranslations} from "next-intl/server";
+import {NextIntlClientProvider} from "next-intl";
+import {pick} from "lodash";
 
 export default async function UserStarter() {
   const locale = await getLocale();
   const t = await getTranslations('HomePage');
+  const messages = await getMessages();
 
   return (
     <LandingStarterBase icon={BookTextIcon} title={t('documentation.title')}
                         desc={<span className="text-muted-foreground text-lg">{t('documentation.desc')}</span>}>
       <div className="flex flex-col gap-8 justify-center items-center w-full sm:w-fit">
-        <ModSearch locale={locale} placeholder={t('documentation.search.placeholder')}/>
-
+        <NextIntlClientProvider messages={pick(messages, 'LoadingContent')}>
+          <ModSearch locale={locale} placeholder={t('documentation.search.placeholder')}/>
+        </NextIntlClientProvider>
+          
         <Button asChild className="text-primary-foreground">
           <LocaleNavLink href="/browse">
             <CompassIcon className="w-4 h-4 mr-2"/>
