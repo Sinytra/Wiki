6f40de20f5154978f0715c49482b5254b8d77612
Su5eD <su5ed@outlook.com>
2024-10-09 21:58:38 +0200

    Fix robots file

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,15 @@
 import type { MetadataRoute } from 'next';
+import available from "@/lib/locales/available";
  
 export default function robots(): MetadataRoute.Robots {
+  const languages = Object.keys(available.getAvailableLocales());
+  const disallow = ['/dev/', '/report/', ...languages.flatMap(l => [`/${l}/dev/`, `/${l}/report/`])];
+
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/dev/', '/report/'],
+      disallow: disallow,
     },
     sitemap: process.env.NEXT_APP_URL ? `${process.env.NEXT_APP_URL}/sitemap.xml` : undefined,
   }
