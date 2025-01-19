e5efeca8cf2273e6eaaacfbd2d6d50ca0c4a3102
Su5eD <su5ed@outlook.com>
2025-01-19 18:52:38 +0100

    Change mod version range to Popover

--- components/docs/ModVersionRange.tsx
+++ components/docs/ModVersionRange.tsx
@@ -1,5 +1,5 @@
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
 import {useTranslations} from "next-intl";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 
 export default function ModVersionRange({versions}: { versions: string[] }) {
   const t = useTranslations('ModVersionRange');
@@ -8,20 +8,18 @@ export default function ModVersionRange({versions}: { versions: string[] }) {
     <>
       <span className="text-sm sm:text-base">{versions[versions.length - 1]}</span>
       {versions.length > 1 &&
-        <TooltipProvider>
-            <Tooltip>
-                <TooltipTrigger className="text-sm sm:text-base">
-                  ({t('desc', {count: versions.length - 1})})
-                </TooltipTrigger>
-                <TooltipContent className="max-h-56 max-w-32 overflow-y-auto slim-scrollbar" side="bottom">
-                    <ul>
-                      {...versions.reverse().slice(1).map((s, i) => (
-                        <li key={i}>{s}</li>
-                      ))}
-                    </ul>
-                </TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
+        <Popover>
+            <PopoverTrigger className="text-sm sm:text-base">
+                ({t('desc', {count: versions.length - 1})})
+            </PopoverTrigger>
+            <PopoverContent className="px-3 py-1.5 text-sm w-fit max-h-56 max-w-32 overflow-y-auto slim-scrollbar" side="bottom">
+                <ul>
+                  {...versions.reverse().slice(1).map((s, i) => (
+                    <li key={i}>{s}</li>
+                  ))}
+                </ul>
+            </PopoverContent>
+        </Popover>
       }
     </>
   )
