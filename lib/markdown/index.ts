f8ee25e9cb6c60ed42726cdb3987006f39bc0854
Su5eD <su5ed@outlook.com>
2025-03-15 16:11:38 +0100

    Improve markdown sanitization

--- lib/markdown/index.ts
+++ lib/markdown/index.ts
@@ -97,22 +97,24 @@ const mdxElemets = ['mdxJsxFlowElement', 'mdxJsxTextElement'];
 
 function sanitizeHastTree(tree: any, components: any) {
   if (mdxElemets.includes(tree.type)) {
-    if (components[tree.name] !== undefined || markdownRehypeSchema.tagNames!.includes(tree.name)) {
+    if (components[tree.name] !== undefined) {
       return tree;
     }
-    return null;
+    if (!tree.name || !markdownRehypeSchema.tagNames!.includes(tree.name)) {
+      return null;
+    }
+  } else {
+    if (tree.tagName && !markdownRehypeSchema.tagNames!.includes(tree.tagName)) {
+      return null;
+    }
   }
 
   let sanitized = tree;
-  if (tree.tagName && !markdownRehypeSchema.tagNames!.includes(tree.tagName)) {
-    return null;
-  }
   if (tree.children) {
     sanitized.children = tree.children
       .map((c: any) => sanitizeHastTree(c, components))
       .filter((c: any) => c != null);
   }
-
   return sanitized;
 }
 
