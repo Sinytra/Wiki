0fcd2830b32d38adebd78df10fc577463cc754d7
Su5eD <su5ed@outlook.com>
2025-08-16 12:59:00 +0200

    Whitelist files in previewer
    Thanks .DS_Store

--- packages/previewer/src/localFiles.ts
+++ packages/previewer/src/localFiles.ts
@@ -4,18 +4,26 @@ import {promises as fs} from 'fs';
 import url from 'url';
 import {LocalDocumentationFile, LocalDocumentationSource} from './localDocsPages';
 
+function isFileTypeAllowed(f: DirectoryTree): boolean {
+  const ALLOWED_FILE_TYPES = ['.mdx', '.json'];
+
+  return f.type === 'directory' || ALLOWED_FILE_TYPES.some(t => f.name.endsWith(t));
+}
+
 async function readFileTree(source: LocalDocumentationSource): Promise<FileTree> {
   const tree = dirTee(`${source.path}`, {attributes: ['type']})?.children || [];
   return convertDirectoryTree(tree);
 }
 
 function convertDirectoryTree(tree: DirectoryTree[]): FileTree {
-  return tree.map(e => ({
-    name: e.name,
-    path: e.path,
-    type: e.type === 'directory' ? 'dir' : 'file',
-    children: e.children ? convertDirectoryTree(e.children) : []
-  }));
+  return tree
+    .filter(isFileTypeAllowed)
+    .map(e => ({
+      name: e.name,
+      path: e.path,
+      type: e.type === 'directory' ? 'dir' : 'file',
+      children: e.children ? convertDirectoryTree(e.children) : []
+    }));
 }
 
 async function readFileContents(source: LocalDocumentationSource, path: string): Promise<LocalDocumentationFile> {
@@ -26,13 +34,15 @@ async function readFileContents(source: LocalDocumentationSource, path: string):
 
 async function readShallowFileTree(source: LocalDocumentationSource, path: string): Promise<FileTree> {
   const tree = dirTee(`${source.path}/${path}`, {attributes: ['type'], depth: 1});
-  return (tree?.children || []).map(f => ({
-    name: f.name,
-    path: f.path,
-    type: f.type === 'directory' ? 'dir' : 'file',
-    children: [],
-    url: url.pathToFileURL(source.path).toString()
-  }));
+  return (tree?.children || [])
+    .filter(isFileTypeAllowed)
+    .map(f => ({
+      name: f.name,
+      path: f.path,
+      type: f.type === 'directory' ? 'dir' : 'file',
+      children: [],
+      url: url.pathToFileURL(source.path).toString()
+    }));
 }
 
 export default {
