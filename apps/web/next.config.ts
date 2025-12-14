6d9d35a0edf7a7ba7ec9e631e62ca9ff94d09240
Su5eD <su5ed@outlook.com>
2025-12-14 22:11:30 +0100

    Remove cache header override

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,36 +3,16 @@ import createMDX from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
 import {withContentlayer} from 'next-contentlayer2';
 import {type CodeHikeConfig} from 'codehike/mdx';
-import locales from '@repo/shared/locales';
 import type {NextConfig} from 'next';
 
 const withNextIntl = createNextIntlPlugin(
   './src/components/i18n/request.tsx'
 );
 
-const intlMatcher = locales.getLanguagePaths().join('|');
-
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [{hostname: 'media.forgecdn.net'}, {hostname: 'cdn.modrinth.com'}]
-  },
-  async headers() {
-    return [
-      {
-        source: `/((?:${intlMatcher})/)?(mod|project|about)/:path*`,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=604800, stale-while-revalidate=86400',
-          },
-          {
-            key: 'CDN-Cache-Control',
-            value: 's-maxage=31536000, stale-while-revalidate=86400',
-          }
-        ]
-      }
-    ]
   },
   // Backwards compatibility only
   async redirects() {
