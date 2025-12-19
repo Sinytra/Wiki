6b0b80605a508788ca2eda899fc2055a6218b54d
Su5eD <su5ed@outlook.com>
2025-12-19 18:02:50 +0100

    Fix failing realpath in buildscript

--- build-previewer.sh
+++ build-previewer.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 BUILD="build/previewer"
-ORIG_BUILD=$(realpath apps/previewer)
-ORIG_APP=$(realpath apps/web)
-OUTPUT=$(realpath build/output)
+OUTPUT="build/output"
+SRC_PATH=$(realpath apps/previewer)
+APP_PATH=$(realpath apps/web)
 
 # Create build folder
 rm -rfd "$BUILD"
 rm -rfd "$OUTPUT"
 mkdir -p "$BUILD"
+
+OUTPUT_PATH=$(realpath "$OUTPUT")
 
 # Copy source
 rsync -avh --progress \
@@ -31,11 +34,11 @@ rsync -avh --progress \
 
 rsync -avh --progress \
   --exclude='tsconfig.json' \
-  $ORIG_BUILD/ \
+  $SRC_PATH/ \
   "$BUILD"
 
 # Override deps
-jq --slurpfile root "$ORIG_APP/package.json" '
+jq --slurpfile root "$APP_PATH/package.json" '
   .dependencies = $root[0].dependencies |
   .devDependencies = $root[0].devDependencies
 ' "$BUILD/package.json" > "$BUILD/package.json.tmp" && mv "$BUILD/package.json.tmp" "$BUILD/package.json"
@@ -45,6 +48,8 @@ cd "$BUILD"
 npm pkg set name="@sinytra/wiki-previewer"
 # Set app version
 VERSION=$(git describe --tags --long --match "v[0-9]*.[0-9]*" | sed -E 's/^v?([0-9]+\.[0-9]+)\.[0-9]+-([0-9]+)-g.*/\1.\2/')
+
+echo "Detected version: $VERSION"
 npm pkg set version="$VERSION"
 npm pkg set scripts.build="next build"
 
@@ -55,16 +60,17 @@ pnpm install
 pnpm run build
 
 # Grab output
-cp -r public .next/standalone/$BUILD/ && cp -r .next/static .next/standalone/$BUILD/.next/
-cp -a .next/standalone/$BUILD $OUTPUT/
-cp -a $ORIG_BUILD/bin $OUTPUT/
+cp -a public ".next/standalone/$BUILD/"
+cp -a .next/static ".next/standalone/$BUILD/.next/"
+cp -a ".next/standalone/$BUILD" "$OUTPUT_PATH"
+cp -a "$SRC_PATH/bin" "$OUTPUT_PATH"
 
 # Filter dependencies
-cd "$OUTPUT"
+cd "$OUTPUT_PATH"
 DEPS_TO_COPY='["concurrently", "chokidar", "ws", "next", "react", "react-dom", "require-in-the-middle", "import-in-the-middle"]'
 
 # Filter production dependencies
-jq --argjson list "$DEPS_TO_COPY" --slurpfile src "$ORIG_APP/package.json" '
+jq --argjson list "$DEPS_TO_COPY" --slurpfile src "$APP_PATH/package.json" '
   .dependencies = (
     $src[0].dependencies
     | with_entries(select(.key as $k | $list | index($k)))
@@ -72,7 +78,7 @@ jq --argjson list "$DEPS_TO_COPY" --slurpfile src "$ORIG_APP/package.json" '
 ' package.json > package.json.tmp && mv package.json.tmp package.json
 
 # Add production dependencies from apps/previewer
-jq --argjson list "$DEPS_TO_COPY" --slurpfile src "$ORIG_BUILD/package.json" '
+jq --argjson list "$DEPS_TO_COPY" --slurpfile src "$SRC_PATH/package.json" '
   .dependencies += (
     $src[0].dependencies
     | with_entries(select(.key as $k | $list | index($k)))
