e82d6ae2e043b03978b731b8af166aab549d6e78
Su5eD <su5ed@outlook.com>
2025-12-29 14:46:47 +0100

    Update middleware matcher

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -7,11 +7,11 @@ import {USER_COUNTRY_COOKIE} from "@/lib/cookies";
 const handleI18nRouting = createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?:dev|report).*|(?!api|blog|(?:en|de|fr|es|it|cs|hu|pl|ms|ms_Ar|tr|sv|uk|ru|ja|ko|zh_cn|zh_tw)(?:(?!/dev|/report)|$)|_vercel/insights|_vercel/speed-insights|_next/static|_next/image|static/.+|sinytra-wiki.schema.json|_meta.schema.json|opengraph-image|favicon.ico|robots.txt|sitemap.xml|.*\\.png$).*)'],
+  matcher: ['/((?:admin|dev|report).*|(?!api|blog|(?:en|de|fr|es|it|cs|hu|pl|ms|ms_Ar|tr|sv|uk|ru|ja|ko|zh_cn|zh_tw)(?:(?!/admin|/dev|/report)|$)|_vercel/insights|_vercel/speed-insights|_next/static|_next/image|static/.+|opengraph-image|favicon.ico|robots.txt|sitemap.xml|.*\\.png$).*)'],
 }
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-  if (request.nextUrl.pathname.match(/^(\/[^/]+)?\/(?:dev|report)/)) {
+  if (request.nextUrl.pathname.match(/^(\/[^/]+)?\/(?:dev|admin|report)/)) {
     const localResp = handleI18nRouting(request);
     if (localResp.status !== 200) {
       return localResp;
