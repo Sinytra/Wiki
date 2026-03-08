d320a7df918af6405b158e1a6d2cc4f5857b9068
moritz-htk <hartkopfmoritz5@gmail.com>
2026-03-08 14:48:26 +0100

    Add German author guide translation

---
+++ apps/docs/content/de/docs/folder_structure/_meta.tsx
@@ -0,0 +1,10 @@
+export default {
+  'overview': 'Übersicht',
+   'wiki_meta': '',
+   'pages': '',
+   'folder_meta': '',
+   'homepage': 'Startseite',
+   'assets': '',
+   'translations': '',
+   'game_data': ''
+};
