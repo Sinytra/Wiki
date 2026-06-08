3e4beb6656cc51812f93047a1c17381b5a0c7005
Su5eD <su5ed@outlook.com>
2026-06-08 11:26:45 +0200

    Fix compile error

--- apps/web/src/app/[locale]/(main)/(developers)/auth/refresh/route.ts
+++ apps/web/src/app/[locale]/(main)/(developers)/auth/refresh/route.ts
@@ -1,12 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { SESSION_KEY } from '@/lib/authSession';
 import { cookies } from 'next/headers';
+import env from '@repo/shared/env';
 
 export async function GET(request: NextRequest) {
   const response = NextResponse.redirect(new URL('/auth/login', request.nextUrl));
 
   const store = await cookies();
-  store.delete({ name: SESSION_KEY, domain: process.env.AUTH_COOKIE_DOMAIN });
+  const sessionKey = env.getSessionCookieName();
+  store.delete({ name: sessionKey, domain: process.env.AUTH_COOKIE_DOMAIN });
 
   return response;
 }
