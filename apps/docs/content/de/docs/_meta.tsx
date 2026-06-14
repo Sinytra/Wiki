6b1075c7dda3d5069f4b610c4b9fa7c4edf8299b
Kyronis <73389489+moritz-htk@users.noreply.github.com>
2026-06-14 19:41:57 +0200

    Add German author guide translation (#150)
    * Add German author guide translation
    
    * Update German author guide translation part 1
    
    * Update German author guide translation part 2
    
    * Final improvements

---
+++ apps/docs/content/de/docs/_meta.tsx
@@ -0,0 +1,34 @@
+export default {
+    '#1': {
+        type: 'separator',
+        title: 'Autorenleitfaden'
+    },
+    introduction: 'Einleitung',
+    getting_started: 'Erste Schritte',
+    '#2': {
+        type: 'separator'
+    },
+    folder_structure: 'Ordnerstruktur',
+    '#3': {
+        type: 'separator'
+    },
+    tools: 'Werkzeuge & Integration',
+    '#4': {
+        type: 'separator'
+    },
+    portal: {
+        title: 'Wiki-Portal',
+        theme: {
+            collapsed: true
+        }
+    },
+    '#5': {
+        type: 'separator'
+    },
+    components: {
+        title: 'Komponenten',
+        theme: {
+            collapsed: true
+        }
+    }
+};
---
+++ apps/docs/content/de/docs/folder_structure/_meta.tsx
@@ -0,0 +1,10 @@
+export default {
+  overview: 'Übersicht',
+  wiki_meta: '',
+  pages: '',
+  folder_meta: '',
+  docs_index: '',
+  assets: '',
+  translations: '',
+  game_data: ''
+};
